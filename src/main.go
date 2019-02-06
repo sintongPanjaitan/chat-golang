ea7a170186beb6211bc7c6005fadf2c05eda27bb
sintong panjaitan <sintongjait@gmail.com>
2019-02-06 22:51:24 +0700

    delete port

--- src/main.go
+++ src/main.go
@@ -36,11 +36,11 @@ func main() {
 	go handleMessages()
 
 	// Start the server on localhost port 8000 and log any errors
-	log.Println("http server started on :8000")
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+//	log.Println("http server started on :8000")
+//	err := http.ListenAndServe(":8000", nil)
+//	if err != nil {
+//		log.Fatal("ListenAndServe: ", err)
+//	}
 }
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
